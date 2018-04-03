a03d44469f889b9a9a4b74c69d908fcdc34781d6
laudena <laudena@gmail.com>
2018-04-03 10:14:24 +0300

    css playing around

---
+++ server/http.js
@@ -0,0 +1,28 @@
+//var basePath = __dirname + "/..";
+//var http = require('http');
+//var fs = require('fs');
+//var path = require('path');
+//
+//http.createServer(function(req, res) {
+//    var stream = fs.createReadStream(path.join(basePath, req.url));
+//    stream.on('error', function() {
+//        res.writeHead(404);
+//        res.end();
+//    });
+//    stream.pipe(res);
+//}).listen(8080);
+
+const server=require('node-http-server');
+
+server.deploy(
+    {
+        port:8080,
+        root:__dirname + "/.."
+    },
+    serverReady
+);
+
+
+function serverReady(server){
+   console.log( `Server on port ${server.config.port} is now up`);
+}
