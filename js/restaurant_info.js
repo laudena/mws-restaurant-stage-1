47a6e3cd5b349d9f9bf1989b020fc6b5fc2ca16b
laudena <laudena@gmail.com>
2018-06-02 20:49:34 +0300

    Revert "image resize added"
    This reverts commit c370f905c887bb7e4b80a3da0e3229fbc0885a59.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,8 +58,6 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.srcset = DBHelper.imageSrcsetForRestaurant(restaurant);
-
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
