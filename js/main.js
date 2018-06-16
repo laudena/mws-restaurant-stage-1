7887fa00ea6d4cbdde5237eacaaceedc8a228ebb
laudena <laudena@gmail.com>
2018-06-16 09:45:17 +0300

    attempting offline 1st
    not too good

--- js/main.js
+++ js/main.js
@@ -12,6 +12,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
   fetchCuisines();
 });
 
+
+
+//import IndexController from './IndexController';
+//const polyfillsNeeded = [];
+//
+//if (!('Promise' in self)) polyfillsNeeded.push('/js/polyfills/promise.js');
+//
+//try {
+//  new URL('b', 'http://a');
+//}
+//catch (e) {
+//  polyfillsNeeded.push('/js/polyfills/url.js');
+//}
+
+//loadScripts(polyfillsNeeded, function() {
+  //new IndexController(document.querySelector('.main'));
+//});
+
+
+
+
+
 /**
  * Fetch all neighborhoods and set their HTML.
  */
---
+++ js/sw/index.js
@@ -0,0 +1,81 @@
+var staticCacheName = 'rest-static-v1';
+var contentImgsCache = 'rest-content-imgs';
+var allCaches = [
+  staticCacheName,
+  contentImgsCache
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/skeleton',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'js/dbhelper.js',
+        'css/styles.css',
+        'index.html',
+        'restaurant.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('rest-') &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+    if (requestUrl.pathname.startsWith('/img/')) {
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+function servePhoto(request) {
+  var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
+
+  return caches.open(contentImgsCache).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
